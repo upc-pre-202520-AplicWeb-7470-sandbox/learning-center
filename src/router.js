09d1811731bf886b6d12b82276ae7683d3a76c87
Rafael C <rafael.castro@upc.edu.pe>
2025-09-24 02:54:53 -0500

    chore: update app.vue, main.js and style.css; add i18n.js and router.js.

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import {createRouter, createWebHistory} from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
+
+// TODO: Define lazy-loaded components for routes
+const about = () => import('./shared/presentation/views/about.vue');
+const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
+const routes = [
+    { path: '/home', name: 'home', component: Home, meta: { title: 'Home' } },
+    { path: '/about', name: 'about', component: about, meta: { title: 'About' } },
+    { path: '/', redirect: '/home' },
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page Not Found' } }
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes,
+
+});
+
+router.beforeEach((to, from, next) => {
+    console.log(`Navigating from ${from.name} to ${to.name}`);
+    // Set the page title
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} - ${to.meta['title']}`;
+    // TODO: Call authentication guard
+    next();
+});
+
+export default router;
